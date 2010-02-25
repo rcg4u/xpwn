cff87bd5bebaf083abe743f90727d59213653fae
iH8sn0w <ih8sn0wydaz>
2010-02-24 18:51:04 -0700

    Added some minor fixes for 3.x Firmwares.

--- ipsw-patch/itunespwn.c
+++ ipsw-patch/itunespwn.c
@@ -9,7 +9,7 @@
 
 #define SHGFP_TYPE_CURRENT 0
 
-const char restorePlist[] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n<plist version=\"1.0\">\n<dict>\n	<key>DeviceClass</key>\n	<string>iPhone</string>\n	<key>DeviceMap</key>\n	<array/>\n	<key>FirmwareDirectory</key>\n	<string>Firmware</string>\n	<key>ProductBuildVersion</key>\n	<string>5A348</string>\n	<key>ProductType</key>\n	<string>iPhone1,1</string>\n	<key>ProductVersion</key>\n	<string>2.0</string>\n	<key>SupportedProductTypeIDs</key>\n	<dict>\n		<key>DFU</key>\n		<array>\n			<integer>304218112</integer>\n		</array>\n		<key>Recovery</key>\n		<array/>\n	</dict>\n</dict>\n</plist>\n";
+const char restorePlist[] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n<plist version=\"1.0\">\n<dict>\n	<key>DeviceClass</key>\n	<string>iPhone</string>\n	<key>DeviceMap</key>\n	<array/>\n	<key>FirmwareDirectory</key>\n	<string>Firmware</string>\n <key>KernelCachesByPlatform</key>\n <dict/>\n	<key>ProductBuildVersion</key>\n	<string>7A341</string>\n	<key>ProductType</key>\n	<string>iPhone1,1</string>\n	<key>ProductVersion</key>\n	<string>3.0</string>\n <key>RamDisksByPlatform</key>\n <dict/>\n     <key>SupportedProductTypeIDs</key>\n	<dict>\n		<key>DFU</key>\n		<array>\n			<integer>304218112</integer>\n		</array>\n		<key>Recovery</key>\n		<array/>\n	</dict>\n</dict>\n</plist>\n";
 
 
 int main(int argc, char* argv[]) {
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]) {
 		mkdir(path, 0755);
 	}
 
-	strcat(path, "\\x12220000_4_Recovery.ipsw");
+	strcat(path, "\\x12220000_5_Recovery.ipsw");
 
 	void* buffer;
 	buffer = malloc(sizeof(restorePlist) - 1);
